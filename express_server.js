01453558d163e88719ba7ce6a49dd9a6753bc69c
A E <aelmollaz@gmail.com>
2021-12-07 11:01:16 +0000

    Added new url endpoint and installed body-parser

--- express_server.js
+++ express_server.js
@@ -22,6 +22,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase.shortURL};
   res.render("urls_show", templateVars);
@@ -30,6 +34,7 @@ app.get("/urls/:shortURL", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
