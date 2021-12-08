c663d708f65701c58f392bfc3b106af27482fed3
A E <aelmollaz@gmail.com>
2021-12-08 19:00:01 +0000

    Added comments to express_server

--- express_server.js
+++ express_server.js
@@ -6,14 +6,25 @@ const cookieParser = require('cookie-parser');
 
 app.set("view engine", "ejs");
 
+//Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieParser());
 
+//Data
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {  //Generates a random 6 digit string
+  let result = "";
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 6; i ++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+//Routes
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,7 +38,7 @@ app.get("/urls", (req, res) => {  //Renders the all the urls in urlDatabase
   res.render("urls_index", templateVars);
 });
 
-app.get("/urls/new", (req, res) => {
+app.get("/urls/new", (req, res) => {  //Renders a page to create a new shortUrl
   const templateVars = {urls: urlDatabase, username: req.cookies.username};
   res.render("urls_new", templateVars);
 });
@@ -46,11 +57,11 @@ app.get("/u/:shortURL", (req, res) => {  //Redirects to longURL page directly
   res.redirect(longURL);
 });
 
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+app.post("/urls", (req, res) => {   //Generates shortUrl for a given longUrl and saves it to urlDatabase object
+  console.log(req.body);  //Logs the shortURL:longURL pair in request body to the console
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${shortURL}`);
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => { //Deletes url enteries and redirects
@@ -63,25 +74,16 @@ app.post("/urls/:shortURL", (req, res) => { //Edits shortURL to assign a new lon
   res.redirect("/urls");
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", (req, res) => {  //Sets a new cookie with the username value
   const username = req.body.username;
   res.cookie("username", username);
   res.redirect("/urls");
 });
 
-app.post("/logout", (req, res) => {
+app.post("/logout", (req, res) => {  //Clears the saved cookie
   res.clearCookie("username");
   res.redirect("/urls");
 });
-
-function generateRandomString() {  //Generates a random 6 digit string
-  let result = "";
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (let i = 0; i < 6; i ++) {
-    result += characters.charAt(Math.floor(Math.random() * characters.length));
-  }
-  return result;
-}
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
